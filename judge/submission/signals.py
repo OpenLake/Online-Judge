d2f690975c15d0ac8c889e6de3a4073a5513f1b5
shubham goyal <22shubh22@gmail.com>
2022-01-06 06:04:38 +0530

    feat: setup celery

--- judge/submission/signals.py
+++
@@ -1,6 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispath mport receiver
-from .models import Submission
-
-@receiver(post_save, sender=Submission)
-def run_code_in_background
