76a57489260c172cdf9175d28825575f3b059831
shubham goyal <22shubh22@gmail.com>
2022-01-09 12:19:24 +0530

    feat: code check function

--- judge/submission/task.py
+++ judge/submission/task.py
@@ -1,11 +1,12 @@
 from celery import shared_task
-from .models import Submission
+from .models import Submission, SubmissionTest
 from question.models import Question, Test
 import sys
+import json
 
 @shared_task
 def queue_submission(submission_id):
-    submission = Submission.object.get(id=submission_id)
+    submission = Submission.objects.get(id=submission_id)
     submission.status = "processing"
     submission.save()
     code_file_path = submission.code_file.path
@@ -16,13 +17,28 @@ def queue_submission(submission_id):
         spec = importlib.util.spec_from_file_location("code." + str(filename), str(code_file_path))
         foo = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(foo)
+
         # pass parameters for submit, from test file of question
-        for test_obj in Test.objects.filter(question=submission.question):
-            input_test_file = test_obj.input_file
-            foo.submit()
-    # get the codefile
-    # run the code file with list of test case
-    #   if invalid file format, SumissionTest status is NA.
-    #   if run for more than 2 mnin, TLE
-    #   if run but incorrrect answer, status is WA (create output_file)
-    #
+        temp_submission_status = "AC"
+        for test_obj in Test.objects.filter(question=submission.question.id):
+            input_test = json.loads(test_obj.test)
+            print(input_test)
+            print(type(input_test))
+            # run the code 
+            output = foo.submit(*input_test)
+            # create a submit tet object
+            submit_test = SubmissionTest.objects.create(test=test_obj, submission=submission, output=json.dumps(output))
+            if submit_test.output == test_obj.correct_answer:
+                submit_test.status = "AC"
+                submit_test.save()
+            elif submit_test.output != "" and submit_test.output != test_obj.correct_answer:
+                submit_test.status = "WA"
+                submit_test.save()
+                temp_submittion_status = "WA"
+        submission.status = temp_submission_status
+        submission.save()
+        return "No run time error"
+    except Exception as e:
+        error = str(e)
+        print("run time error")
+        return error
